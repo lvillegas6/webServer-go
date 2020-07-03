70734c37acab6e975304907c6cb50e782fdafebd
Luis Andres Villegas <luis.villegas@unisimon.edu.co>
2020-07-03 18:44:45 -0500

    Rutas

--- router.go
+++ router.go
@@ -1,22 +1,34 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
+//Router ...
 type Router struct {
 	rules map[string]http.HandlerFunc
 }
 
+//NewRouter ...
 func NewRouter() *Router {
 	return &Router{
 		rules: make(map[string]http.HandlerFunc),
 	}
 }
 
+//FindHandler ...
+func (r *Router) FindHandler(path string) (http.HandlerFunc, bool) {
+	handler, exist := r.rules[path]
+	return handler, exist
+}
+
+//ServeHTTP ...
 func (r *Router) ServeHTTP(write http.ResponseWriter, request *http.Request) {
+	handler, exist := r.FindHandler(request.URL.Path)
 
-	fmt.Fprintf(write, "Hola mundo")
-
+	if !exist {
+		write.WriteHeader(http.StatusNotFound)
+		return
+	}
+	handler(write, request)
 }
