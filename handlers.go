a3e7073f6d1707ee61bcc6b4898adb9424a90b9f
Luis Andres Villegas <luis.villegas@unisimon.edu.co>
2020-07-05 19:51:12 -0500

    handlers

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -12,4 +13,36 @@ func HandleRoot(writer http.ResponseWriter, r *http.Request) {
 
 func HandleHome(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello from the very best server!")
+}
+
+func PostRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var metaData MetaData
+	err := decoder.Decode(&metaData)
+
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Payload %v\n", metaData)
+}
+
+func UserPostRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	err := decoder.Decode(&user)
+
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+	response, err := user.toJson()
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-type", "application/json")
+	w.Write(response)
+
 }
