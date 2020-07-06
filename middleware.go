68420d447677a25a5303d00f1cabb133849b461a
Luis Andres Villegas <luis.villegas@unisimon.edu.co>
2020-07-05 19:51:01 -0500

    Middleware

--- middleware.go
+++ middleware.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
-func CheckAuth() Middleware {
-	return func(f http.HandlerFunc) http.HandlerFunc {
-		return func(w http.ResponseWriter, r *http.Request) {
-			flag := true
-			fmt.Println("Checking authentication")
-			if flag {
-				f(w, r)
-			} else {
-				return
-			}
+func CheckAuth(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		flag := true
+		fmt.Println("Checking authentication")
+		time.Sleep(2 * time.Second)
+		if flag {
+			f(w, r)
+		} else {
+			return
 		}
+	}
+}
+
+func Logging(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		defer func() {
+			log.Println(r.URL.Path, time.Since(start))
+		}()
+		f(w, r)
 	}
 }
